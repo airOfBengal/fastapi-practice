935b5b2830b3662e58aaecbc02aaaabcc1a74aee
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2025-07-07 10:41:59 +0600

    update gitignore

--- schemas.py
+++ schemas.py
@@ -1,4 +1,32 @@
 from pydantic import BaseModel
+from typing import List
+
+class User(BaseModel):
+    id: int
+    username: str
+    class Config():
+        orm_mode = True
+
+class ArticleBase(BaseModel):
+    title: str
+    content: str
+    published: bool
+    creator_id: int
+
+class ArticleDisplay(BaseModel):
+    title: str
+    content: str
+    published: bool
+    user: User
+    class Config():
+        orm_mode = True
+
+class Article(BaseModel):
+    title: str
+    content: str
+    published: bool
+    class Config():
+        orm_mode = True
 
 class UserBase(BaseModel):
     username: str
@@ -8,6 +36,7 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
     username: str
     email: str
+    items: List[Article] = []
     class Config():
         orm_mode = True
 
